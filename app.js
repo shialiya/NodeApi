41625bf05062fb04498ee0c5c067b881e5b49bea
yiranzhemeliu <1714978050@qq.com>
2024-11-02 05:39:32 +0900

    初始化

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use('/web', express.static('web'));
+
+const PORT = process.env.PORT || 55555;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+
+
+});
+
+const {  getdata} = require('./BattleSrp.js');
+
+app.post('/getBattleLogin', bodyParser.json(), bodyParser.urlencoded({ extended: false }),async  function(req, res) {
+    var password = req.body.password;
+    var srpParams = req.body.srpParams;
+    res.json(getdata(srpParams,password));
+
+});
