ec24c8ecf4645205432055aa91e682a5888b8989
yiranzhemeliu <1714978050@qq.com>
2025-07-23 02:57:34 +0800

    提交kameleo浏览器的操作

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const url = require('url');
 
 app.use(cors());
 app.use('/web', express.static('web'));
@@ -14,6 +15,7 @@ app.listen(PORT, () => {
 });
 
 const adsService = require('./service/AdsService.js');
+const kameleoService = require('./service/KameleoService.js');
 const linkenService = require('./service/LinkenService.js');
 
 const {  getdata} = require('./BattleSrp.js');
@@ -234,8 +236,7 @@ app.get('/adsStopBrowser', bodyParser.json(), bodyParser.urlencoded({ extended: 
 app.get('/adsDeleteUser', bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
     try {
         const user_ids=  req.query.user_ids;
-        const json=  await adsService.deleteUser(user_ids);
-        res.send(json);
+        await adsService.deleteUser(user_ids);
         console.log(user_ids+"删除窗口"+new Date());
 
     } catch (error) {
@@ -280,6 +281,156 @@ app.get('/linkenStopDelete', bodyParser.json(), bodyParser.urlencoded({ extended
     } catch (error) {
         // 捕获异常并打印错误信息和接收到的 body
         console.error("adsCreateUser错误", error);
+        console.log("Received body:", req.body);  // 打印接收到的 body 信息
+
+        // 返回一个错误响应，但不直接终止程序
+        res.status(500).json({ error: "错误" });
+    }
+});
+
+app.post("/proxy", async (req, res) => {
+    const {
+        url,
+        method = "GET",
+        headers = {},
+        proxy,
+        body,
+        browser = "chrome_124"
+    } = req.body;
+
+    try {
+        const options = {
+            method: method.toUpperCase(),
+            url: url,
+            headers: headers,
+            timeout: 30000,
+            tlsClientIdentifier: browser || "chrome_124", // 如 chrome_120, chrome_124, firefox_120
+            followRedirects: true
+        };
+
+        // 设置请求体
+        if (["POST", "PUT", "PATCH"].includes(options.method) && body) {
+            options.body = typeof body === "object" ? JSON.stringify(body) : body;
+        }
+
+        // 处理代理
+        if (proxy) {
+            options.proxyUrl = `http://${proxy.trim()}`;
+        }
+
+        // 发起请求
+        const response = await request(options);
+
+        res.json({
+            responseBody: response.body,
+            responseHeaders: response.headers,
+            statusCode: response.statusCode,
+            cookieStore: response.cookies
+        });
+    } catch (err) {
+        console.error("❌ 请求失败:", err);
+        res.status(500).json({
+            error: err.message || "请求失败",
+            stack: err.stack
+        });
+    }
+});
+/**
+ * kameleo 查询符合条件的所有指纹
+ */
+app.get('/queryKameleFingerprints', bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
+    try {
+        // 获取完整的 query string 字符串，不包含前面的 '?'
+        const queryString = url.parse(req.originalUrl).query;
+
+        // 将整个 queryString 作为 browserProduct 传入
+        const browserProduct = queryString;
+
+        const json = await kameleoService.queryFingerprints(browserProduct);
+
+        res.send(json);
+        console.log(browserProduct + " 查询指纹 " + new Date());;
+
+    } catch (error) {
+        // 捕获异常并打印错误信息和接收到的 body
+        console.error("adsStopBrowser错误", error);
+        console.log("Received body:", req.body);  // 打印接收到的 body 信息
+
+        // 返回一个错误响应，但不直接终止程序
+        res.status(500).json({ error: "错误" });
+    }
+});
+
+/**
+ * kameleo 创建浏览器实例
+ */
+app.post('/kameleoCreateUser' , bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
+    try {
+        const json= await  kameleoService.createUser(req.body);
+        res.send(json);
+        console.log(JSON.stringify(json) +"创建窗口"+new Date());
+
+    } catch (error) {
+        // 捕获异常并打印错误信息和接收到的 body
+        console.error("adsCreateUser错误", error);
+        console.log("Received body:", req.body);  // 打印接收到的 body 信息
+
+        // 返回一个错误响应，但不直接终止程序
+        res.status(500).json({ error: "错误" });
+    }
+});
+/**
+ * kameleo 打开窗口
+ */
+app.get('/kameleoOpen', bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
+    try {
+
+        const browserId = req.query.browserId;  // 使用 query 获取 GET 请求中的参数
+        console.log(browserId+"打开窗口窗口"+new Date());
+        const json=    await kameleoService.open(browserId);
+        res.send(json);
+    } catch (error) {
+        // 捕获异常并打印错误信息和接收到的 body
+        console.error("adsOpen错误", error);
+        console.log("Received body:", req.body);  // 打印接收到的 body 信息
+
+        // 返回一个错误响应，但不直接终止程序
+        res.status(500).json({ error: "错误" });
+    }
+});
+/**
+ * kameleo 关闭浏览器
+ */
+app.get('/kameleoStop', bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
+
+    try {
+
+        const browserId=  req.query.browserId;
+        const json=   await kameleoService.stopBrowser(browserId);
+        res.send(json);
+        console.log(browserId+"关闭窗口"+new Date());
+
+    } catch (error) {
+        // 捕获异常并打印错误信息和接收到的 body
+        console.error("adsStopBrowser错误", error);
+        console.log("Received body:", req.body);  // 打印接收到的 body 信息
+
+        // 返回一个错误响应，但不直接终止程序
+        res.status(500).json({ error: "错误" });
+    }
+});
+/**
+ * kameleo 删除窗口
+ */
+app.get('/kameleoDeleteUser', bodyParser.json(), bodyParser.urlencoded({ extended: false }), async function(req, res) {
+    try {
+        const browserId=  req.query.browserId;
+        await kameleoService.deleteUser(browserId);
+        console.log(user_ids+"删除窗口"+new Date());
+
+    } catch (error) {
+        // 捕获异常并打印错误信息和接收到的 body
+        console.error("deleteUser错误", error);
         console.log("Received body:", req.body);  // 打印接收到的 body 信息
 
         // 返回一个错误响应，但不直接终止程序
---
+++ service/KameleoService.js
@@ -0,0 +1,312 @@
+var host = "http://127.0.0.1:";
+var port = 5050;
+var urlQian = host + port
+const axios = require('axios').default;
+const fetch = require('fetch-retry')(require('node-fetch'), {
+    retries: 1,
+    retryDelay: (attempt, error, response) => Math.pow(2, attempt) * 1000,
+    fetchTimeout: 60000,  // 超时时间为8000毫秒（5秒）
+    retryOn: (attempt, error, response) => {
+
+        if (attempt >= 2) {
+            console.error(`达到最大重试次数返回false (${attempt}).`);
+            return false;
+        }
+        // 重试条件
+        if (error) {
+            return [
+                'ECONNRESET',
+                'ETIMEDOUT',
+                'ENOTFOUND',
+                'EAI_AGAIN',
+                'Client network socket disconnected before secure TLS connection was established'
+            ].includes(error.code || error.message);
+        }
+        // 检查响应状态码
+        if (response && response.status === 502) {
+            console.error(`Attempt ${attempt + 1} received 502 status code`);
+            return true; // 如果返回502，进行重试
+        }
+        return false;
+    }
+});
+/**
+ * 创建创空
+ * @param data
+ * @returns {Promise<unknown>}
+ */
+class KameleoService {
+
+    // constructor() {
+    //     this.queue = [];  // 队列，用于保存待调用的函数
+    //     this.isProcessing = false;  // 标志是否正在处理请求
+    // }
+    // async _callWithDelay(func) {
+    //     return new Promise((resolve, reject) => {
+    //         this.queue.push({ func, resolve, reject });
+    //         this._processQueue();  // 尝试处理队列中的请求
+    //     });
+    // }
+    //
+    // // 处理队列中的请求
+    // async _processQueue() {
+    //     if (this.isProcessing) return;  // 如果已经在处理请求，直接返回
+    //     if (this.queue.length === 0) return;  // 如果队列为空，直接返回
+    //
+    //     this.isProcessing = true;  // 设置正在处理状态
+    //
+    //     // 每次调用一个函数，并在三秒后处理下一个
+    //     const { func, resolve, reject } = this.queue.shift();
+    //     try {
+    //         const result = await func();  // 调用队列中的函数
+    //         resolve(result);  // 调用成功，返回结果
+    //     } catch (error) {
+    //         reject(error);  // 调用失败，返回错误
+    //     }
+    //
+    //     // 等待2秒后继续处理队列中的下一个请求
+    //     setTimeout(() => {
+    //         this.isProcessing = false;
+    //         this._processQueue();  // 继续处理下一个请求
+    //     }, 1100);  // 延迟 1.1 秒钟
+    // }
+
+    constructor() {
+        this.queue = [];  // 队列，用于保存待调用的函数
+        this.intervalId = null;  // 用于定时调用的定时器ID
+    }
+
+    // 添加任务到队列
+    async _callWithDelay(func) {
+        return new Promise((resolve, reject) => {
+            this.queue.push(async () => {
+                try {
+                    const result = await func();  // 调用传入的异步函数
+                    resolve(result);  // 返回结果
+                } catch (error) {
+                    reject(error);  // 错误处理
+                }
+            });
+        });
+    }
+
+    // 开始定时调用请求
+    startProcessing() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);  // 清除已有的定时器
+        }
+
+        this.intervalId = setInterval(() => {
+            if (this.queue.length > 0) {
+                const func = this.queue.shift();  // 获取队列中的第一个函数
+                func();  // 调用它
+            }
+        }, 2000);  // 每 1.8 秒钟执行一次
+    }
+
+    // 停止定时调用
+    stopProcessing() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);  // 清除定时器
+            this.intervalId = null;
+        }
+    }
+
+    async createUser(data) {
+        return this._callWithDelay(() => this._createUser(data));
+    }
+    //创建窗口
+    async _createUser(data) {  // 这个函数接收一个参数 data
+        try {
+            const response = await fetch(`${urlQian}/profiles/new`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data) // 将 data 转为 JSON 字符串
+            });
+
+            if (!response.ok) {
+                // 如果响应状态码不是 2xx，抛出错误
+                const errorText = await response.text(); // 获取错误响应文本
+                throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+            }
+
+            // 返回 JSON 格式的响应体
+            const result = await response.json();
+            return result;
+        } catch (error) {
+            // 捕获错误并抛出
+            throw error;
+        }
+    }
+    async open(profileId) {
+        return this._callWithDelay(() => this._open(profileId));
+    }
+
+    /**
+     * 打开窗口
+     * @param profileId 配置id
+     * @returns {Promise<{success: boolean, message: string}|{success: boolean, message: *}|any>}
+     */
+    async _open(profileId) {
+
+        let responseJson =   null;
+        let  responseBody =  null;
+        try {
+            const resp = await fetch(`${urlQian}/profiles/${profileId}/start`, {
+                method: 'POST'
+            });
+            const res=await resp.json();
+            responseBody = JSON.stringify(res);
+            if (res.lifetimeState) {
+                // const browser = await puppeteer.connect({browserWSEndpoint: res.data.data.ws.puppeteer, defaultViewport: null});
+                // const page = await browser.newPage();
+                // await page.goto('https://www.adspower.com');
+                // await page.screenshot({ path: './adspower.png' });
+                // await browser.close();
+                return res;
+            } else {
+                return {success: false, message: "_open."+responseBody+"当前浏览器为"+profileId};
+            }
+        } catch (err) {
+            console.log(err.message+"ads返回"+responseBody);
+            return {success: false, message: err.message};
+        }
+    }
+
+    async queryFingerprints(profileId) {
+        return this._callWithDelay(() => this._queryFingerprints(profileId));
+    }
+
+    /**
+     * 查询符合条件的所有指纹
+     * @param profileId 配置id
+     * @returns {Promise<{success: boolean, message: string}|{success: boolean, message: *}|any>}
+     */
+    async _queryFingerprints(profileId) {
+
+        let responseJson =   null;
+        let  responseBody =  null;
+        try {
+            const resp = await fetch(`${urlQian}/fingerprints?${profileId}`, {
+                method: 'GET'
+            });
+            const res=await resp.json();
+
+
+            responseBody = JSON.stringify(res);
+            if (Array.isArray(res)) {
+                // const browser = await puppeteer.connect({browserWSEndpoint: res.data.data.ws.puppeteer, defaultViewport: null});
+                // const page = await browser.newPage();
+                // await page.goto('https://www.adspower.com');
+                // await page.screenshot({ path: './adspower.png' });
+                // await browser.close();
+                return res;
+            } else {
+                return {success: false, message: "获取所有指纹库失败"+responseBody+"当亲传参为"+profileId};
+            }
+        } catch (err) {
+            console.log(err.message+"ads返回"+responseBody);
+            return {success: false, message: err.message};
+        }
+    }
+    async stopBrowser(profileId) {
+        return this._callWithDelay(() => this._stopBrowser(profileId));
+    }
+    /**
+     * 关闭窗口
+     * @param profileId 配置id
+     * @returns {Promise<null|any>}
+     */
+    async _stopBrowser(profileId) {
+        const url = `${urlQian}/profiles/${profileId}/stop`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',  // 使用 GET 方法
+                headers: {}  // 请求头
+            });
+
+            // 返回 JSON 格式的响应
+            return await response.json();
+        } catch (error) {
+            console.log("关闭窗口报错"+error);
+            return null;
+        }
+    }
+
+    async deleteUser(user_ids) {
+        return this._callWithDelay(() => this._deleteUser(user_ids));
+    }
+    /**
+     * 删除窗口
+     * @param user_ids
+     * @returns {Promise<null|any>}
+     */
+    async _deleteUser(user_ids) {
+
+
+
+        const url = `${urlQian}/profiles/${user_ids}`;
+
+        // 配置请求头
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        try {
+            // 发起初始请求
+            let response = await fetch(url, {
+                method: 'DELETE',
+                headers: headers
+            });
+
+            return null;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+
+    async updateUser(user_ids) {
+        return this._callWithDelay(() => this._updateUser(user_ids));
+    }
+    //修改指纹信息
+    async _updateUser(data) {  // 这个函数接收一个参数 data
+        try {
+
+            const response = await fetch(`${urlQian}/api/v1/user/update`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data) // 将 data 转为 JSON 字符串
+            });
+
+            if (!response.ok) {
+                // 如果响应状态码不是 2xx，抛出错误
+                const errorText = await response.text(); // 获取错误响应文本
+                throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+            }
+
+            // 返回 JSON 格式的响应体
+            const result = await response.json();
+            return result;
+        } catch (error) {
+            // 捕获错误并抛出
+            throw error;
+        }
+    }
+
+}
+var kameleoService= new KameleoService();
+
+kameleoService.startProcessing();
+
+module.exports = kameleoService;
+// async  function text(){
+//    var ss= await adsService.deleteUser("kqxxww5");
+//    console.log(ss);
+// }
+// text();
